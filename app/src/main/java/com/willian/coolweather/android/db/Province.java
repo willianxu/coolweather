ea71594f7b3e3d7f586c8ebeadec5d409f4cc3f0
willianxu <wlxu_163@163.com>
2017-10-29 15:39:57 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/willian/coolweather/android/db/Province.java
@@ -0,0 +1,10 @@
+package com.willian.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by willian on 2017/10/29.
+ */
+
+public class Province extends DataSupport {
+}
