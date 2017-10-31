0d900fa62ecd1d91e0462cddc5958d528696fba5
willianxu <wlxu_163@163.com>
2017-10-31 09:52:24 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/willian/coolweather/android/ChooseAreaFragment.java
@@ -0,0 +1,10 @@
+package com.willian.coolweather.android;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by willian on 2017/10/29.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+}
---
+++ app/src/main/java/com/willian/coolweather/android/util/Utility.java
@@ -0,0 +1,8 @@
+package com.willian.coolweather.android.util;
+
+/**
+ * Created by willian on 2017/10/29.
+ */
+
+public class Utility {
+}
